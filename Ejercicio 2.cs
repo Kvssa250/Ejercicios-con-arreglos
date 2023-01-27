773c83f3ae4f6524c236f4c8ce56c4f930772499
Kvssa250 <122424551+Kvssa250@users.noreply.github.com>
2023-01-26 22:26:34 -0400

    Add files via upload

---
+++ Ejercicio 2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            List<int> even_numbers = new List<int>();
+            int cantidad = 0;
+            int mayor = 0;
+            int z = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Escriba el valor no. {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            foreach (int x in numbers)
+            {
+                if (x % 2 == 0)
+                { 
+                    even_numbers.Add(x);
+                    cantidad++;
+                }
+            }
+            while (z < cantidad)
+            {
+                if (mayor < even_numbers[z])
+                {
+                    mayor = even_numbers[z];
+                }
+                z++;
+            }
+            for (int y = 0; y < numbers.Length; y++)
+            {
+                if (mayor == numbers[y])
+                {
+                    Console.WriteLine($"El numero par mas grande es: {mayor}, y esta en la posicion no. {y + 1}.");
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio 3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            List<int> prime_numbers = new List<int>();
+            int cantidad = 0;
+            int mayor = 0;
+            int z = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Escriba el valor no. {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            foreach (int x in numbers)
+            {
+                if (x == 2)
+                {
+                    prime_numbers.Add(x);
+                    cantidad++;
+                }
+                for (int q = 2; q < x; q++)
+                {
+                    if (x % q == 0)
+                    {
+                        break;
+                    }
+                    if (x % q != 0)
+                    {
+                        prime_numbers.Add(x);
+                        cantidad++;
+                        break;
+                    }
+                }
+            }
+            while (z < cantidad)
+            {
+                if (mayor < prime_numbers[z])
+                {
+                    mayor = prime_numbers[z];
+                }
+                z++;
+            }
+            for (int y = 0; y < numbers.Length; y++)
+            {
+                if (mayor == numbers[y])
+                {
+                    Console.WriteLine($"El numero primo mas grande es: {mayor}, y esta en la posicion no. {y + 1}.");
+                }
+            }
+        }
+    }
+}
