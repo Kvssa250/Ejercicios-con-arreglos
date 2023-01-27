28e299ba91b540c327ca5681e430efd69957c03e
Kvssa250 <122424551+Kvssa250@users.noreply.github.com>
2023-01-26 22:50:25 -0400

    Add files via upload

---
+++ Ejercicio 7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            int sum = 0;
+            int cantidad = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Escriba el valor no. {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            foreach (int x in numbers)
+            {
+                sum = sum + x;
+                cantidad++;
+            }
+            float average = sum / cantidad;
+            Console.WriteLine($"El promedio de los numeros de la lista es: {average}");
+        }
+    }
+}
